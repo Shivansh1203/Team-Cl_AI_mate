155f11c67b2afe5a612c3609f0f6c73ab273971c
HarshHrs24 <107180900+HarshHrs24@users.noreply.github.com>
2023-02-24 00:13:08 +0530

    sampletest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import warnings
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 import numpy as np
 import keras
@@ -7,9 +9,12 @@ import pandas as pd
 import tensorflow as tf
 import streamlit as st
 from plotly import graph_objs as go
+from prophet.plot import *
+from prophet.serialize import model_to_json, model_from_json
 # from PIL import Image
 # import requests
 # from io import BytesIO
+
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title("Sample websapp")
@@ -19,96 +24,25 @@ selected_city = st.selectbox('Select dataset for prediction', cities)
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model = tf.keras.models.load_model('content/ann_youtubeadview.h5')
-  return model
+  with open('content/serialized_model.json', 'r') as fin:
+    m = model_from_json(fin.read())  # Load model
+  return m
 
 with st.spinner('Loading Model Into Memory....'):
-  model = load_model()
+  m= load_model()
 
 data_load_state = st.text('Loading data...')
 
-#testing part
-data_test = pd.read_csv("content/test.csv") 
-data_test=data_test[data_test.views!='F']
-data_test=data_test[data_test.likes!='F']
-data_test=data_test[data_test.dislikes!='F']
-data_test=data_test[data_test.comment!='F']
-category={'A': 1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8}
-data_test["category"]=data_test["category"].map(category)
+future = m.make_future_dataframe(periods = 365)
+forecast = m.predict(future)
 
-# Convert values to integers for views, likes, comments, dislikes and adview
-data_test["views"] = pd.to_numeric(data_test["views"])
-data_test["comment"] = pd.to_numeric(data_test["comment"])
-data_test["likes"] = pd.to_numeric(data_test["likes"])
-data_test["dislikes"] = pd.to_numeric(data_test["dislikes"])
-column_vidid=data_test['vidid']
-
-# Endoding features like Category, Duration, Vidid
-from sklearn.preprocessing import LabelEncoder
-data_test['duration']=LabelEncoder().fit_transform(data_test['duration'])
-data_test['vidid']=LabelEncoder().fit_transform(data_test['vidid'])
-data_test['published']=LabelEncoder().fit_transform(data_test['published'])
-
-# Convert Time_in_sec for duration
-import datetime
-import time
-def checki(x):
-  y = x[2:]
-  h = ''
-  m = ''
-  s = ''
-  mm = ''
-  P = ['H','M','S']
-  for i in y:
-    if i not in P:
-      mm+=i
-    else:
-      if(i=="H"):
-        h = mm
-        mm = ''
-      elif(i == "M"):
-        m = mm
-        mm = ''
-      else:
-        s = mm
-        mm = ''
-  if(h==''):
-    h = '00'
-  if(m == ''):
-    m = '00'
-  if(s==''):
-    s='00'
-  bp = h+':'+m+':'+s
-  return bp
-
-train=pd.read_csv("test.csv")
-mp = pd.read_csv(path)["duration"]
-time = mp.apply(checki)
-
-def func_sec(time_string):
-  h, m, s = time_string.split(':')
-  return int(h) * 3600 + int(m) * 60 + int(s)
-
-time1=time.apply(func_sec)
-
-data_test["duration"]=time1
-data_test=data_test.drop(["vidid"],axis=1)
-
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
-X_test = data_test
-X_test=scaler.fit_transform(X_test)
-
-prediction = model.predict(X_test)
-prediction=pd.DataFrame(prediction)
-prediction = prediction.rename(columns={0: "Adview"})
-
-
-
-data_load_state.text('Loading data... done!')
 
 st.subheader('predicted data')
-st.write(prediction.tail())
+st.write(forecast.tail())
+
+st.subheader('graph')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
 
 # def plot_raw_data(data):
 # 	fig = go.Figure()
@@ -135,3 +69,7 @@ st.write(prediction.tail())
 #     st.write("")
 #     image = Image.open(BytesIO(content))
 #     st.image(image, caption='Classifying Bean Image', use_column_width=True)
+
+
+
+
