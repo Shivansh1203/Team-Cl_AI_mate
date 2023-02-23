f5fe554273e0dbee2b8673f85ca63d4de5c5b91f
HarshHrs24 <107180900+HarshHrs24@users.noreply.github.com>
2023-02-24 00:25:49 +0530

    sample3

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 import numpy as np
-import keras
+
 import pandas as pd
 import tensorflow as tf
 import streamlit as st
 
 from prophet.plot import plot_plotly
-# from prophet.plot import *
-from prophet.serialize import model_to_json, model_from_json
-# from PIL import Image
-# import requests
-# from io import BytesIO
+
+from prophet.serialize import  model_from_json
+
 
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -44,33 +42,3 @@ st.write(forecast.tail())
 st.subheader('graph')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
-
-# def plot_raw_data(data):
-# 	fig = go.Figure()
-# 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-# 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-# 	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-# 	st.plotly_chart(fig)
-	
-# plot_raw_data(prediction)
-
-# def decode_img(image):
-#   img = tf.image.decode_jpeg(image, channels=3)  
-#   img = tf.image.resize(img,[224,224])
-#   return np.expand_dims(img, axis=0)
-
-# path = st.text_input('Enter Image URL to Classify.. ','https://beanipm.pbgworks.org/sites/pbg-beanipm7/files/styles/picture_custom_user_wide_1x/public/AngularLeafSpotFig1a.jpg')
-# if path is not None:
-#     content = requests.get(path).content
-
-#     st.write("Predicted Class :")
-#     with st.spinner('classifying.....'):
-#       label =np.argmax(model.predict(decode_img(content)),axis=1)
-#       st.write(classes[label[0]])    
-#     st.write("")
-#     image = Image.open(BytesIO(content))
-#     st.image(image, caption='Classifying Bean Image', use_column_width=True)
-
-
-
-
